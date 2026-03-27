7892127d7f20938461a7b22ddfbfb9902ab94e00
quocnva <quocnva@runsystem.net>
2026-03-27 09:25:14 +0700

    first commit: Portfilo simple page

---
+++ script.js
@@ -0,0 +1,9 @@
+function showAlert() {
+    alert("Thanks for reaching out! 😊");
+}
+
+function scrollToSection(sectionId) {
+    document.getElementById(sectionId).scrollIntoView({
+        behavior: "smooth"
+    });
+}
