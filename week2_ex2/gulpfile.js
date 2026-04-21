d8399d4d3eb4a8c77ffbdad7ea0d62ea7dedd35b
quocnva-09 <quocnva@runsystem.net>
2026-04-21 15:48:36 +0700

    week2_ex2: ver1.0

---
+++ week2_ex2/gulpfile.js
@@ -0,0 +1,20 @@
+const { src, dest, watch, series } = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+
+function buildStyles() {
+  return (
+    src("src/scss/**/*.scss")
+      //compress scss to css
+      .pipe(sass({ outputStyle: "compressed" }).on("error", sass.logError))
+      //export ra folder build/css
+      .pipe(dest("build/css"))
+  );
+}
+
+function watchTask() {
+  //thiết lập trigger thay đổi scss sang css khi có thay đổi
+  watch(["src/scss/**/*.scss"], series(buildStyles));
+}
+
+//tạo lệnh default cho npx gulp với 2 hàm truyền vào
+exports.default = series(buildStyles, watchTask);
